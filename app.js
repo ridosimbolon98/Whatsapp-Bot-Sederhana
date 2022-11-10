9e8cb3d5618bd35a05b63a3754831256e892951d
Rido Simbolon <ridosimbolon98@gmail.com>
2022-11-10 13:52:09 +0700

    Membuat API Kirim Pesan

---
+++ app.js
@@ -0,0 +1,137 @@
+const { Client, MessageMedia, LocalAuth, Buttons, List } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
+const { phoneNumberFormatter, phoneNumberReformatter } = require('./helpers/formatter');
+const fileUpload = require('express-fileupload');
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(fileUpload({
+  debug: false
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
+});
+
+
+
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  authStrategy: new LocalAuth()
+});
+
+
+
+
+client.initialize();
+
+client.on('message', async msg => {
+  switch (msg) {
+    case "PING":
+      client.sendMessage(msg.from, "Whatsapp Bot Online");
+      break;
+  
+    default:
+      client.sendMessage(msg.from, "Maaf, Bot ini masih dalam pengembangan!.\n*Rido Martupa*");
+      break;
+  }
+});
+
+// Socket IO
+io.on('connection', function(socket) {
+  socket.emit('message', 'Connecting...');
+
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code received, scan please!');
+    });
+  });
+
+  client.on('ready', () => {
+    socket.emit('ready', 'Whatsapp is ready!');
+    socket.emit('message', 'Whatsapp is ready!');
+  });
+
+  client.on('authenticated', () => {
+    socket.emit('authenticated', 'Whatsapp is authenticated!');
+    socket.emit('message', 'Whatsapp is authenticated!');
+    console.log('AUTHENTICATED');
+  });
+
+  client.on('auth_failure', function(session) {
+    socket.emit('message', 'Auth failure, restarting...');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'Whatsapp is disconnected!');
+    client.destroy();
+    client.initialize();
+  });
+});
+
+
+const checkRegisteredNumber = async function(number) {
+  const isRegistered = await client.isRegisteredUser(number);
+  return isRegistered;
+}
+
+const findGroupByName = async function(name) {
+  const group = await client.getChats().then(chats => {
+    return chats.find(chat => 
+      chat.isGroup && chat.name.toLowerCase() == name.toLowerCase()
+    );
+  });
+  return group;
+}
+
+// send message
+app.post('/kirim_pesan', async (req, res) => {
+  const nomor = phoneNumberFormatter(req.body.nomor);
+  const pesan = req.body.pesan;
+
+  client.sendMessage(nomor, pesan).then(response => {
+      res.status(200).json({
+        status: true,
+        response: response
+      });
+  }).catch(err => {
+      res.status(500).json({
+        status: false,
+        response: err
+      });
+  });
+});
+
+server.listen(port, function() {
+  console.log('Bot running on *: ' + port);
+});
