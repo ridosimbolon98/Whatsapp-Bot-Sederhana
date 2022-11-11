da5cd6eff024da674df3382ce330185e6488704b
Rido C Martupa Simbolon <39256115+ridosimbolon98@users.noreply.github.com>
2022-11-11 14:38:26 +0700

    Tambah fitur kirim gambar

--- app.js
+++ app.js
@@ -127,6 +127,27 @@ app.post('/kirim_pesan', async (req, res) => {
   });
 });
 
+// kirim gambar
+app.post('/kirim_gambar', async(req, res) => {
+    const nomor = phoneNumberFormatter(req.body.nomor);
+    const media = await MessageMedia.fromUrl(req.body.url);
+    const caption = req.body.caption;
+
+    client.sendMessage(nomor, media, {
+      caption: caption
+    }).then(response => {
+      res.status(200).json({
+        status: true,
+        response: response
+      });
+    }).catch(err => {
+      res.status(500).json({
+        status: false,
+        response: err
+      });
+    });
+});
+
 server.listen(port, function() {
   console.log('Bot running on *: ' + port);
 });
